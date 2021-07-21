d648fd2874d65fc16540ae069cf225f84830af01
Jason Viox <jrv04@hotmail.com>
2021-07-21 11:25:47 -0500

    add realPath null and asynce create newNote

--- Develop/app/db/service.js
+++ Develop/app/db/service.js
@@ -1,8 +1,20 @@
 import { promises as fs } from "fs";
 
-let realPath;
+let realPath = null;
 
-export default {};
+export default {
+    async index() {
+        return JSON.parse(await fs.readFile(`${realPath)/app/db/db.json`, "utf8"));
+};
+
+async create(newNote {
+    const currentNotes = await this.index();
+    fs.writeFile(
+        `$(realPath)/app/db/db.json`,
+        JSON.stringify([...currentNotes, newNote])
+        );
+    };
+};
 
 (async () => {
     realPath = await fs.realpath("./");
